ea9a8a369498da80acd6bae2bd7ca22a325ceeab
vasylnadtochii <vasyl@stayopn.com>
2022-11-25 17:21:11 +0200

    Initial Commit

---
+++ Crypto_App_iOS/Crypto_App_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_App_iOSApp.swift
+//  Crypto_App_iOS
+//
+//  Created by Vasyl Nadtochii on 25.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_App_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
